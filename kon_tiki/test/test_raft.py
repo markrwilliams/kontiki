8d2ade23625eb8c720f1f3379148ece18a6fff74
matthewnorman <mnorman.hep@gmail.com>
2014-10-30 21:37:32 -0700

    First tests for voting and candidate term checking

--- kon_tiki/test/test_raft.py
+++ kon_tiki/test/test_raft.py
@@ -32,3 +32,48 @@ class RaftServerTest(unittest.TestCase):
         self.assertFalse(server.candidateIdOK(candidateId=ID))
         server.persister.votedFor = 'some_ID'
         self.assertTrue(server.candidateIdOK(candidateId=ID))
+
+
+    def test_candidateLogUpToDate(self):
+        """
+        Is the candidate up to date? The term and the index together
+        have to be at least as advanced as that of the current machine.
+
+        """
+
+        server = self.create_server()
+        currentTerm = 100
+        log = []
+        for x in xrange(10):
+            log.append(persist.LogEntry(term=currentTerm, command=x))
+
+        server.persister.currentTerm = currentTerm
+        server.persister.log = log
+        print server.candidateLogUpToDate(lastLogIndex=len(log),
+                                          lastLogTerm=currentTerm)
+
+
+    def test_requestVote(self):
+        """
+        Do I give you a vote? It depends.
+
+        1) Is your term less than mine? If so, the answer is no.
+        2) Have I voted for you before, and candidate log is up to date
+        """
+        server = self.create_server()
+
+        currentTerm = 100
+        candidateId = 'ThisCandidate'
+        lastLogIndex = 10
+        lastLogTerm = 100
+
+        # Test for term
+        server.persister.currentTerm = 101
+        term, vg = server.remote_requestVote(term=currentTerm,
+                                             candidateId=candidateId,
+                                             lastLogIndex=lastLogIndex,
+                                             lastLogTerm=lastLogTerm)
+        self.assertEquals(term, 101)
+        self.assertFalse(vg)
+
+        
