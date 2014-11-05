060b65c51770a829c25d244cab11c51d66e91839
matthewnorman <mnorman.hep@gmail.com>
2014-11-04 22:05:53 -0800

    Checkpoint commit for raft_candidate while DelayedCall is being worked on

---
+++ kon_tiki/test/test_raft_candidate.py
@@ -0,0 +1,36 @@
+from kon_tiki import raft
+from kon_tiki import persist
+from twisted.trial import unittest
+
+
+class RaftCandidateTest(unittest.TestCase):
+
+    def create_candidate(self):
+        persister = persist.ListPersist()
+        identity = 'identity'
+        peers = set(['peer1', 'peer2', 'peer3', 'peer4', 'peer5'])
+        applyCommand = None
+        timeoutRange = (.150, .350)
+        cycle = raft.ServerCycle(identity=identity, peers=peers,
+                                 applyCommand=applyCommand,
+                                 persister=persister)
+        def fake_conduct(*args):
+            pass
+        self.patch(raft.Candidate, 'conductElection', fake_conduct)
+        candidate = raft.Candidate(identity=identity, cycle=cycle,
+                                   peers=peers, applyCommand=applyCommand,
+                                   electionTimeoutRange=timeoutRange,
+                                   persister=persister)
+        return candidate
+
+    def test_prepareForElection(self):
+        """
+        Just make sure things are set up
+
+        """
+
+        candidate = self.create_candidate()
+        candidate.prepareForElection()
+        self.assertEqual(candidate.votes, 0)
+        self.assertEqual(candidate.persister.votedFor, 'identity')
+        self.assertEqual(candidate.persister.currentTerm, 1)
