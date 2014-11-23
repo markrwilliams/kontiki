2aea029d66419a3f3d348960feb147ea9d9fe36a
Mark Williams <markrwilliams@gmail.com>
2014-11-23 02:53:13 -0800

    remove duplicate ReconnectingPBClientFactory code

--- kontiki/fundamentals.py
+++ kontiki/fundamentals.py
@@ -1,5 +1,3 @@
-from twisted.spread.pb import PBClientFactory
-from twisted.internet.protocol import ReconnectingClientFactory
 from heapq import heapify, heappop
 
 
@@ -15,19 +13,3 @@ def majorityMedian(l):
     if not l:
         raise ValueError("no median for empty data")
     return nlargest(len(l) / 2 + 1, l)[-1]
-
-
-class ReconnectingPBClientFactory(PBClientFactory,
-                                  ReconnectingClientFactory):
-
-    def clientConnectionLost(self, connector, unused_reason):
-        PBClientFactory.clientConnectionLost(self,
-                                             connector,
-                                             unused_reason,
-                                             reconnecting=True)
-        ReconnectingPBClientFactory.clientConnectionLost(self,
-                                                         connector,
-                                                         unused_reason)
-
-    def clientConnectionFailed(self, connector, reason):
-        PBClientFactory.clientConnectionFailed(self, connector, reason)
